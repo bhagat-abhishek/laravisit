39cdf81425c4654fa5913ad6d31658b333bc68bd
bhagat-abhishek <85464874+bhagat-abhishek@users.noreply.github.com>
2023-03-22 06:27:03 +0530

    Vendor Publish Implemented
    my first contribution - the vendor publish was not working so i tried to fix it.

--- src/LaravisitServiceProvider.php
+++ src/LaravisitServiceProvider.php
@@ -7,6 +7,20 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class LaravisitServiceProvider extends PackageServiceProvider
 {
+    
+    public function boot()
+    {
+        // Publish a config file
+        $this->publishes([
+            __DIR__.'/../config/laravisit.php' => config_path('laravisit.php')
+        ]);
+
+        // Publish your migrations
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_laravisits_table.php.stub' => database_path('/migrations/'.date('Y_m_d_His', time()). '_create_laravisits_table.php')
+        ]);
+    }
+    
     public function configurePackage(Package $package): void
     {
         /*
@@ -19,4 +33,5 @@ class LaravisitServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasMigration('create_laravisits_table');
     }
+    
 }
